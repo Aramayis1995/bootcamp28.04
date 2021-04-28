4bf8c0f791a36850545be294aa7459b762259866
Aramayis <mayissargsyan1995@gmail.com>
2021-04-28 21:12:29 +0400

    Check the login

---
+++ task_8.js
@@ -0,0 +1,15 @@
+let username = prompt("enter your username", "");
+if (username === "admin") {
+  let passward = prompt("enter your passward", "");
+  if (passward === "TheMaster") {
+    alert("Welcome!");
+  } else if (passward !== "TheMaster" && passward !== "" && passward !== null) {
+    alert("Wrong password");
+  } else {
+    alert("Canceled");
+  }
+} else if (username === "" || username === null) {
+  alert("Canceled");
+} else {
+  alert("I don’t know you");
+}
