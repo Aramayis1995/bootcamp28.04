8322ec588d9ccaeca2fe30f5fa46f24e979558ec
Aramayis <mayissargsyan1995@gmail.com>
2021-04-28 18:13:56 +0400

    task if:or:and

---
+++ task_7.js
@@ -0,0 +1,6 @@
+if (-1 || 0) alert("first");
+//true: qani vor tesnume -1 vor@ veradarcnume true ev qani vor kam-i nshane el araj chi gnum ev ardyunq@ veradarcnume true.
+if (-1 && 0) alert("second");
+//false: tesnume -1 vor@ veradarcnume true , bayc qani vor ev-i nshne ancnume araj ev tesnum 0 vor@ veradarcnume false ev ardyunq@ veradarcnume fals.
+if (null || (-1 && 1)) alert("third");
+//true: tesnume null vor@ veradarcnume false bayc qani vor kam-i nshane ancnume araj ev tesnum (-1 && 1) vor@ veradarcnume 1(true) , ev ardyunq@ veradarcnume true.
